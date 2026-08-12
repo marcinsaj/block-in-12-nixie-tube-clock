0962a898b1506895c2c4512e960cdb478eda5e0b
Marcin Saj <marcin@nixietester.com>
2026-08-12 20:41:57 +0200

    Update test.ino

--- firmware/test.ino
+++ firmware/test.ino
@@ -30,7 +30,6 @@ Programming Procedure:
 #include <avr/pgmspace.h> // (included with Arduino IDE)
 #include <avr/wdt.h>      // (included with Arduino IDE)
 
-
 // Serial debugging 1-ON, 0-OFF
 #define DEBUG 0
 
@@ -43,6 +42,16 @@ const int TX_PIN = PIN_PD1;
 SoftwareSerial mySerial(RX_PIN, TX_PIN);
 #endif
 
+// ============================================================================
+// NIXIE TUBE DIGITS
+// ============================================================================
+const uint8_t tube_bit_map[4][10] = {
+  // 0,  1,  2,  3,  4,  5,  6,  7,  8,  9    // Digits
+  {  8,  7,  6,  5,  4,  3,  2,  1,  0,  9 }, // Tube 1 Hours
+  { 18, 17, 16, 15, 14, 13, 12, 11, 10, 19 }, // Tube 2 Hours
+  { 28, 27, 26, 25, 24, 23, 22, 21, 20, 29 }, // Tube 3 Minutes
+  { 48, 47, 46, 45, 44, 43, 42, 41, 40, 49 }  // Tube 4 Minutes
+};
 
 // ============================================================================
 // PIN DECLARATIONS
